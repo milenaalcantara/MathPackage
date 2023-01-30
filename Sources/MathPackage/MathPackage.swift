10b0bc564dcbc2749b612d9ac62139fad95b9167
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-01-30 14:52:41 -0300

    add sum method

--- Sources/MathPackage/MathPackage.swift
+++ Sources/MathPackage/MathPackage.swift
@@ -1,6 +1,9 @@
 public struct MathPackage {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public init() {}
+    
+    public func sum<T: Numeric>(number x: T, number y: T) -> T {
+        return x + y
     }
 }
